0a9827bd1f367197647e929249e0dd84203ed0e0
Andres Vargas <andresvargasd97@gmail.com>
2024-03-21 10:19:37 -0400

    Versión estable

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -19,5 +19,7 @@ export class DataService {
   getMenuOpts(){
     return this.http.get<Componente[]>('/assets/data/menu-opts.json');
   }
-  
+  getAlbumes(){
+    return this.http.get<any[]>('https://jsonplaceholder.typicode.com/album');
+  }
 }
